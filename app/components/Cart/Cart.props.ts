47d7f122f2c2a352340b5adff52b1dc1d0bb3e14
Yndry <51527655+Yndry@users.noreply.github.com>
2024-03-01 00:40:28 +0300

    start_project

--- app/components/Cart/Cart.props.ts
+++
@@ -1,7 +0,0 @@
-import { DetailedHTMLProps, HTMLAttributes, ReactNode } from "react";
-
-export interface CartProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>{
-	hcontent: string,
-	pcontent: string,
-	
-}
--- app/components/Likebtn/Likebtn.props.ts
+++
@@ -1,5 +0,0 @@
-import { ButtonHTMLAttributes, DetailedHTMLProps } from "react";
-
-export interface LikebtnProps extends DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
-	color: 'colorless' | 'red' | 'blue',
-}
